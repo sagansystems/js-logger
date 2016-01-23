321ba0a77ac50e04d41d9493d9a14be35e5411e1
Gerad Suyderhoud <gerad@sagansystems.com>
2016-01-22 17:55:01 -0800

    remove need for babel

--- jasmine.js
+++ jasmine.js
@@ -1,12 +1,9 @@
-require("babel-core/register")({
-	  presets: ['es2015'] // required for 'import' 
-});
 var Jasmine = require('jasmine');
 
 var jasmine = new Jasmine();
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
 jasmine.configureDefaultReporter({
-    showColors: false
+    showColors: true
 });
 jasmine.execute();
