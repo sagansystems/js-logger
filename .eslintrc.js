8b77173950746d72f7abd02e5effcacb91037fc0
Richard Lee <richard@sagansystems.com>
2016-01-22 18:12:29 -0800

    Linted code with eslint
    linted js code with node, jasmine, and es6 flags on

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            2
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "no-console": 0,
+        "no-unused-vars": [
+          2,
+          {
+            "vars": "all",
+            "args": "none"
+          }
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "node": true,
+        "jasmine": true
+    },
+    "extends": "eslint:recommended"
+};
